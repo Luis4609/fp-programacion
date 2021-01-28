dad8cf21fdb2445e52ffdb1d9071b9f48000f23d
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2021-01-28 15:43:47 +0100

    New Commit Ejercicio10 y Ejercicio11

---
+++ eclipse-workspace/programacion/src/main/java/programacion/ejercicios/tema6/Ejercicio10.java
@@ -0,0 +1,34 @@
+package programacion.ejercicios.tema6;
+
+public class Ejercicio10 {
+
+//	
+//	Escribir un programa para calcular de forma aleatoria una combinación de apuesta a las
+//	quinielas (catorce partidos), pero con la condición de que el número de apuestas a “1”, “X”
+//	y “2” la fijará de antemano el usuario ( con la condición de que su suma sea igual a 14)
+//	
+//	Validación:
+//	• 0<= unos <=14
+//	• 0<= equis <=14
+//	• 0<= doses <=14
+//	• unos + equis+ doses =14
+//	Las condiciones de validación se comprobarán antes de invocar al método, o bien en un
+//	método de validación separado (booleano)
+	
+	/**
+	 * Calcular de forma aleatoria combinacion de apuesta de Quiniela 14 partidas
+	 * @param unos
+	 * @param equis
+	 * @param doses
+	 * @return
+	 */
+	public char[] apuestaQuniela(int unos, int equis, int doses) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
